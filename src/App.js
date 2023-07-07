82aa84d7a75fab847e661f4b1e2752ec0d00fd75
ekram617161 <ekram@opltech.com>
2023-07-07 13:01:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  // second methods  -----------------------------------------------
+
+  // const transformTask = (taskObj) => {
+  //   const loadedTasks = [];
+
+  //   for (const taskKey in taskObj) {
+  //     loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+  //   }
+
+  //   setTasks(loadedTasks);
+  // }
+  // const httpData = useHttp({ url: 'https://647436057de100807b1a7a5b.mockapi.io/users' }, transformTask);
+
+  const httpData = useHttp();
+  const { isLoading, error, sendRequest: fetchTasks } = httpData;
+
+  // first methods  -----------------------------------------------
+
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       'https://647436057de100807b1a7a5b.mockapi.io/users'
+  //     );
+
+  //     if (!response.ok) {
+  //       throw new Error('Request failed!');
+  //     }
+
+  //     const data = await response.json();
+
+  //     const loadedTasks = [];
+
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
+
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || 'Something went wrong!');
+  //   }
+  //   setIsLoading(false);
+  // };
+
+  useEffect(() => {
+    const transformTask = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    }
+    fetchTasks({ url: 'https://647436057de100807b1a7a5b.mockapi.io/users' }, transformTask);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
   );
 }
 
